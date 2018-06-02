83a0855b4c1d9141bd47e720e14cc9e97c04f3eb
Marsel Shiriyazdanov <marsel.shiriyazdanov@flatstack.com>
2018-06-02 12:56:28 +0300

    rails_apps_composer: set up database

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :user do
-    email "MyString"
+    name "Test User"
+    email "test@example.com"
+    password "please123"
+
+    trait :admin do
+      role 'admin'
+    end
+
   end
 end
